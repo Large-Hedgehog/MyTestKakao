6713ff6a2853d1d4cbc0771dcf5a0d178d4b80b9
KDY <kdy12061004@gmail.com>
2023-07-19 23:33:47 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/testkakao/TestKakaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testkakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKakaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestKakaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testkakao/TestKakaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testkakao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestKakaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
